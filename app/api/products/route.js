47e2c3d16b1e4ca37cb6b0061e9a3a374f6b7f3a
Jonathan Doane <johnnydoane13@gmail.com>
2023-05-30 22:52:07 -0700

    progress-3

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,6 +1,7 @@
 
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/product.model";
+import mongoose from "mongoose";
 
 
 
@@ -20,7 +21,18 @@ export async function GET(req) {
 }
 
 
+export async function PUT(req) {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id');
+    await mongooseConnect()
 
+    const updatedFields = await req.json()
+
+    const productUpdated = await Product.findByIdAndUpdate(id, updatedFields, {new:true})
+    console.log(req.body)
+    console.log("here is the update:", productUpdated)
+    return new Response(JSON.stringify(productUpdated))
+}
 
 
 
--- app/products/[id]/edit/page.jsx
+++ app/products/[id]/edit/page.jsx
@@ -3,12 +3,14 @@ import Layout from "@/app/components/layout";
 import { useEffect, useState } from "react";
 import axios from "axios"
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
     export default  function EditProductPage({params}) {
 
         const {id} = params
-        const [products, setProducts] = useState({})
-    
+        const [products, setProducts] = useState({ name: '', description: '', price: ''})
+        //console.log(id)
+
         useEffect(() => {
             axios.get(`/api/products?id=${id}`)
             .then((res) => {setProducts(res.data);console.log(res.data)})
@@ -18,14 +20,26 @@ import Link from "next/link";
         const inputHandler = (e) => {
             setProducts({...products, [e.target.name]: e.target.value })
         }
+        const router = useRouter();
+
+        function UpdateProduct(e){
+            e.preventDefault()
+            
+            axios.put(`/api/products?id=${id}`, products)
+            .then((res)=> {console.log(res.data);router.push('/products')})
+            .catch((err)=> {console.log(err)})
+            
+    
+        }
+
 
     return (
         <Layout>
             <div className="flex w-1/4 justify-between">
                 <h1 className=" bg-blue-900 rounded-lg w-3/4 mt-5 text-center text-white pb-3 p-2 text-4xl">Edit Product</h1>
-                <Link href = {`/products/${id}`} className="simple"> Go Back </Link>
+                <Link href = {'/products/'} className="simple"> Go Back </Link>
                 </div>
-                <form className="flex flex-col">
+                <form onSubmit={UpdateProduct} className="flex flex-col">
                 <label htmlFor="name">Product Name</label>
                 <input type="text" placeholder="Product Name" name="name" value={products.name} onChange={inputHandler}/>
 
@@ -41,3 +55,4 @@ import Link from "next/link";
         </Layout>
     )
 }
+ 
