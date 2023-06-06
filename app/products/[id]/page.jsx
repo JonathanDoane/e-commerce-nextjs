087dc0f5b4ca5460871617e886a4dee1b7ada4a4
Jonathan Doane <johnnydoane13@gmail.com>
2023-06-05 18:30:27 -0700

    update 6/5

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -39,10 +39,10 @@ export default function ViewProductPage({ params }) {
 
             {
                 !loading ? (
-                    <div className="bg-blue-900 rounded-lg text-white text-center text-2xl w-1/3 m-auto mt-8 h-1/4">
+                    <div className="bg-blue-900 rounded-lg text-white text-center text-2xl w-1/2 m-auto mt-8 p-8">
                         <h2 className="mb-5">Price: {products.price}</h2>
-                        <h2>Description: {products.description}</h2>
-                        <img src={`data:image/png;base64,${data}`} alt="" />
+                        <h2 className="mb-5">Description: {products.description}</h2>
+                        <img src={`data:image/png;base64,${data}`} alt="" className="w-1/3 m-auto"/>
                     </div>
                 ) : (
                     <div>
